fc76f07632583e0eb1f8429f74febbe9785cb54b
Kenton Lam <kenton_lam@outlook.com>
2020-12-03 15:21:22 +1000

    Day 3.

--- day03.py
+++ day03.py
@@ -17,19 +17,49 @@ if DEBUG: sys.argv.remove('-v')
 def dprint(*args, **kwargs): 
     if DEBUG: print(*args, **kwargs)
 
-INPUT = 'dayXX_input.txt' if len(sys.argv) == 1 else sys.argv[1]
+INPUT = 'day03_input.txt' if len(sys.argv) == 1 else sys.argv[1]
 
 # import numpy as np 
 # import scipy as sp
 
 def parse(lines: List[str]):
-    pass
+    return lmap(str.strip, lines)
 
 def solve_1(data):
-    pass 
+    r = 0
+    c = 0
+    rows = len(data)
+    cols = len(data[0])
+    x = 0
+    while r < rows:
+        if data[r][c % cols] == '#': x += 1
+        r += 1
+        c += 3
+    return x
 
 def solve_2(data):
-    pass 
+    
+    slopes = [
+        (1, 1),
+        (1, 3),
+        (1, 5),
+        (1, 7),
+        (2, 1)
+    ]
+
+    y = 1
+    for dr, dc in slopes:
+        r = 0
+        c = 0
+        rows = len(data)
+        cols = len(data[0])
+        x = 0
+        while r < rows:
+            if data[r][c % cols] == '#': x += 1
+            r += dr
+            c += dc
+        y *= x
+    return y
 
 if __name__ == "__main__":
     with open(INPUT) as f:
